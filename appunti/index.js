08eb03cd5dc0c85a122d753a56f19c3b90831875
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-07-12 11:23:56 +0200

    add appunti e pulisce codice

--- appunti/index.js
+++ appunti/index.js
@@ -1,6 +1,5 @@
 //!EVENTI
 const saluta = (clickEvent) => {
-  console.log("CIAOOOO!")
   console.log(clickEvent)
 }
 // onkeyup
@@ -9,10 +8,7 @@ const keyUpEs = () => {
   console.log("Quando lascio il pulsante")
 }
 
-//addEventListener
-
 const primoBottone = document.querySelector("button")
-
 // Array.push()
 // Array.pop()
 //esempi di metodi
@@ -37,79 +33,22 @@ primoBottone.addEventListener("click", () => {
 
 //!OGGETTO EVENT
 
-// const sum  = (a,b) => {
-//     return a+b
-// }
-
-// sum(2,3)
-
 const leggiContenuto = (keyboardEvent) => {
   // console.log(keyboardEvent)
   // keyboardEvent.target.value = keyboardEvent.target.value.toUpperCase()
   // console.log(keyboardEvent.target.value)
+  //? se voglio fare qualcosa quando premo "Enter"
   // if(keyboardEvent.key === "Enter") {
   //   console.log(keyboardEvent.target.value)
   // }
 
   document.querySelector("h1").innerHTML = keyboardEvent.target.value
-
-  //Event.target => L'elemento con cui abbiamo interagito
-  // click => button
-  // Event {
-  //   target: {
-  //    il mio bottone
-  // },
-  //....
-  // }
-  //keyup => input
-  /* 
-    Event {
-      target: {
-        ...
-        value: "" => IL CONTENUTO DELL'INPUT
-      },
-      ...
-      key: ""
-    }
-
-
-  */
 }
 
 const cambiaColore = (changeEvent) => {
   console.log(changeEvent)
   document.querySelector("h1").style.color = changeEvent.target.value
 }
-
-//!EVENTI, RECAP:
-
-//? COME APPLICARE GLI EVENTI?
-/* 
-- HTML onclick='fn(event)', onkeyup='fn(event)'
-- JS Element.addEventListener("tipo di evento", fn)
-- JS Element.onclick = fn
-*/
-
-//? COS'E' L'OGGETTO "event"?
-
-/* 
-  Oggetto che rappresenta l'interazione dell'utente
-  Click => cosa ho cliccato, se ho cliccato con il mouse, 
-  se ho cliccato con la penna, in quale parte della pagina,
-  posizione dell'elemento etc... 
-  KeyUp => su quale input ho scritto, se stavo premento shift, ctrl, etc., 
-  cosa ho scritto, il pulsante premuto (Enter, Backspace, "c").
-*/
-
-//? PROPRIETA' IMPORTANTI:
-/* 
-  Element.addEventListener*
-
-  Event.target => L'elemento con cui ho interagito 
-  Event.target.value => Il contenuto dell'input (event.target.value)
-  KeyboardEvent.key => il pulsante premuto
-
-*/
 
 //! FORMS
 
@@ -125,39 +64,25 @@ const cambiaColore = (changeEvent) => {
 //ON SUBMIT => SUL FORM! :)
 const login = (submitEvent) => {
   //refresh => comportamento di default
-  submitEvent.preventDefault()
-  //previene il default === non fa refreshare la pagina
-  //submitEvent => il submit, click o invio
-  //submitEvent.target => il form che abbiamo submittato
-  //submitEvent.target.elements => Array di tutti gli input presenti nel form
-  console.log(submitEvent.target) //oggetto che rappresenta il form 
-  // document.querySelector("form")
+  submitEvent.preventDefault() //previene il default === non fa refreshare la pagina
+  console.log(submitEvent.target) //oggetto che rappresenta il form
+  // oggetto uguale a quello di document.querySelector("form"), ma il form SPECIFICO con cui abbiamo interagito
   const credenziali = {}
-  console.log(submitEvent.target.elements)
+  console.log(submitEvent.target.elements) //Array di tutti gli input disponibili
   for (let i = 0; i < submitEvent.target.elements.length; i++) {
     const input = submitEvent.target.elements[i]
-    console.log(input.id)
-    // credenziali.input.id
+    console.log(input.id) //stampa gli id di tutti gli input
     credenziali[input.id] = input.value //credenziali.password, credenziali.email, credenziali.propic
+    //? loop breakdown, iterazione per iterazione
     //i===0, input.id => "email" => credenziali.email = valore dell'input email
     //i===1, input.id => "password" => credenziali.password = valore dell'input
     //i===2, input.id => "propic" => credenziali.propic = valore dell'input
   }
   console.log(credenziali)
 
+  //! Alternativa, se pochi input
   // const credenziali = {
   //   email: document.querySelector("input#email").value,
   //   password: document.querySelector("input#password").value
   // }
-
-
 }
-
-//! PROPRIETA' DEL FORM
-/*   
-  onsubmit => click button[type='submit'], oppure Enter, VA INSERITO SUL FORM! :)
-
-  SubmitEvent.target => il form che abbiamo submittato
-  Form.element => Array di tutti gli input, tipo [<input/>, <input/>, <input/>]
-
-  */
---
+++ readme.md
@@ -0,0 +1,39 @@
+# EVENTI, RECAP:
+
+## COME APPLICARE GLI EVENTI?
+
+- HTML `onclick='fn(event)'`, `onkeyup='fn(event)'`
+- JS `Element.addEventListener("tipo di evento", fn)`
+- JS `Element.onclick = fn`
+
+## COS'E' L'OGGETTO "event"?
+
+- Oggetto che rappresenta l'interazione dell'utente
+- **Click** => cosa ho cliccato, se ho cliccato con il mouse, se ho cliccato con la penna, in quale parte della pagina, posizione dell'elemento etc...
+- **KeyUp** => su quale input ho scritto, se stavo premento shift, ctrl, etc., cosa ho scritto, il pulsante premuto (Enter, Backspace, "c").
+
+## PROPRIETA' IMPORTANTI:
+
+- (Element.addEventListener)
+
+- `Event.target` => L'elemento con cui ho interagito
+- `Event.target.value` => Il contenuto dell'input (event.target.value)
+- `KeyboardEvent.key` => il pulsante premuto
+
+## FORMS
+
+```html
+<form onsubmit="login(event)">
+  <input type="email" id="email" placeholder="email" />
+  <input type="password" id="password" placeholder="password" />
+  <input type="file" id="propic" />
+
+  <button>Click</button>
+  <!-- button tipo submit -->
+</form>
+```
+
+- onsubmit => click button[type='submit'], oppure Enter, VA INSERITO SUL FORM!
+
+- SubmitEvent.target => il form che abbiamo submittato
+- Form.element => Array di tutti gli input, tipo `[<input/>, <input/>, <input/>]`
